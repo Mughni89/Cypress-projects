823e7d664c7367f5c29b7f998e595041858661f2
Mughni89 <amsyafi86@gmail.com>
2025-06-09 20:44:50 +0700

    Update e2e.ts

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,7 +1,7 @@
 import './commands'
 
-import '../../support/step_definitions/general';
-import '../../support/step_definitions/mentoring';
+import '../../support/step_definitions/general.ts';
+import '../../support/step_definitions/mentoring.ts';
 
 // cypress/support/e2e.ts atau cypress/support/index.ts
 Cypress.on('uncaught:exception', (runnable) => {
